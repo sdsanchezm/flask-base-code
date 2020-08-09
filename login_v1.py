6753b980eed5cf4af79c4f0027aab793f9f4acd9
sdsanchezm <sdsanchezm@gmail.com>
2020-08-08 23:28:50 -0500

    token working now

--- login_v1.py
+++ login_v1.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, request, make_response, 
+from flask import Flask, jsonify, request, make_response
 import jwt
 import datetime
 from functools import wraps
+
+#from: J5bIPtEbS0Q
 
 app = Flask(__name__)
 
@@ -10,7 +12,7 @@ app.config['SECRET_KEY'] = 'thisisthesecretkey21'
 def token_required(f):
 	@wraps(f)
 	def decorated(*args, **kwargs):
-		token = request.args.get(token)
+		token = request.args.get('token')
 
 		if not token:
 			return jsonify({'message':'Token is missing'}), 403
@@ -22,13 +24,14 @@ def token_required(f):
 
 		return f(*args, **kwargs)
 
-	return decorated	
+	return decorated
 
 @app.route('/unprotected')
 def unprotected():
 	return jsonify({'message': 'Anyone can see this'})
 
 @app.route('/protected')
+@token_required
 def protected():
 	return jsonify({'message':'this is available for users with valid tokens'})
 
@@ -36,15 +39,16 @@ def protected():
 def login():
 	auth = request.authorization
 
-	if auth and auth.password == 'password':
-		token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=21)}, app.config['SECRET_KEY'])
+	if auth and auth.password == 'secret1':
+		token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=5)}, app.config['SECRET_KEY'])
 
 		return jsonify({'token': token.decode('UTF-8')})
 
-	return make_response('Could not verify', 401, {'WWW-authenticate':'basic realm="Login Required"'})
+	return make_response('Could not verify', 401, {'WWW-authenticate':'Basic realm="Login Required"'})
 
 
 
 if __name__ == '__main__':
+
 	app.run(debug=True)
 
